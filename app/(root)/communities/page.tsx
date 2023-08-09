29b43b033aa999905156a96916687344ac143cae
Nihar Hegde <niharhegde163@gmail.com>
2023-08-09 16:29:54 +0530

    fixed some minor bugs

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,51 @@
-const Page = async () => {
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import CommunityCard from "@/components/cards/CommunityCard";
+
+//pass params to check the any user whoes id is passed on the params destuere it
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  //TODO : funtion to fetch all the communities here to search between them
+  const results = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
   return (
     <section>
-      <h1 className="head-text mb-10">Communities</h1>
+      <h1 className="head-text mb-10">Search</h1>
+      {/* render a serch bar   */}
+      <div className="mt-14 flex flex-col gap-9">
+        {results.communities.length === 0 ? (
+          <p className="no-result">No communities found</p>
+        ) : (
+          <>
+            {results.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
-};
+}
 
 export default Page;
