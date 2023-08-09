a2b209e8985522dc223ff6a26915b458950015f8
Nihar Hegde <niharhegde163@gmail.com>
2023-08-09 12:45:12 +0530

    fetching all users in search page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,49 @@
-const Page = async () => {
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+//pass params to check the any user whoes id is passed on the params destuere it
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  //TODO : funtion to fetch all the users here to search between them
+  const results = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+      {/* render a serch bar   */}
+      <div className="mt-14 flex flex-col gap-9">
+        {results.users.length === 0 ? (
+          <p className="no-result">No users found</p>
+        ) : (
+          <>
+            {results.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User" //later will make this work for communities too
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
-};
+}
 
 export default Page;
