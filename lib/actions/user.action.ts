447f7fd76d198a7636e82820a83770388d8304cf
Nihar Hegde <niharhegde163@gmail.com>
2023-08-09 13:15:49 +0530

    activity page done

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -125,3 +125,30 @@ export async function fetchUsers({
     throw new Error(`Error while fetching all usres: ${error.message}`)
   }
 }
+
+
+export async function getActivity(userId:string) {
+  try {
+    connectToDB();
+    //*find all threads created by the user
+    const userThreads = await Thread.find({author:userId});
+    //*collect all the child threads ids (replies) from the child fields (basically collect all the comments on origial post)
+    const childThreadIds = userThreads.reduce((acc,userThread)=>{
+      return acc.concat(userThread.children)
+    },[])
+    //?get all replies that are made by the same users
+    //!so that we can ignore the replies by the same user
+    const replies = await Thread.find({
+      _id:{$in:childThreadIds},
+      author:{$ne:userId}
+    }).populate({
+      path:'author',
+      model:User,
+      select:'name image _id'
+    })
+    return replies;
+  } catch (error:any) {
+    throw new Error(`Failed while fetching activitiees: ${error.message}`)
+  }
+  
+}
